5052dd2a8eb279d12e0d563212f875db3ce57605
fukayakenta <fukaya.kentan@gmail.com>
2024-11-12 14:31:31 +0900

    add reset function

--- main.js
+++ main.js
@@ -5,10 +5,17 @@ MAX_CARDNUM = 20
 // 画像表示関連
 document.addEventListener('DOMContentLoaded', function() {
 	showProbability(0)
-    fetch('card_list.csv')
-        .then(response => response.text())
-        .then(data => parseCSV(data));
+
+	const button = document.getElementById('button');
+	button.addEventListener('click', pushResetButton);
+	loadCSV()
 });
+
+function loadCSV() {
+	fetch('card_list.csv')
+	    .then(response => response.text())
+        .then(data => parseCSV(data));
+}
 
 function parseCSV(data) {
     const lines = data.split('\n');
@@ -36,6 +43,13 @@ function showCards(csvLine){
 	}
 }
 
+function removeAllImage(){
+	var images = document.getElementsByTagName('img');
+	while(images.length > 0) {
+    	images[0].parentNode.removeChild(images[0]);
+	}
+}
+
 // 画像クリック
 function imgClick(e){
 	const id = e.target.getAttribute("id")
@@ -49,4 +63,12 @@ function showProbability(num){
 	card_nums = card_nums + num
 	probability = (1/card_nums*100).toFixed(1) 
 	document.getElementById('probability').textContent = probability+"% "+probability*2+"% "
+}
+
+//restボタン
+function pushResetButton(){
+	removeAllImage()
+	card_nums=MAX_CARDNUM
+	showProbability(0)
+	loadCSV()
 }
