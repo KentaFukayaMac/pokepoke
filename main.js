abffb8dc2c3c6cf9ee7cd04e13f3973590cb1339
fukayakenta <fukaya.kentan@gmail.com>
2024-11-12 13:38:20 +0900

    show card list from html

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hello world!!!");
+const GAMEWITH_URL="https://img.gamewith.jp/article_tools/pokemon-tcg-pocket/gacha/"
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('card_list.csv')
+        .then(response => response.text())
+        .then(data => parseCSV(data));
+});
+
+function parseCSV(data) {
+    const lines = data.split('\n');
+    for (let i = 0; i < lines.length; i++) {
+        const cells = lines[i].split(',');
+		if (!cells[0].startsWith("#"))
+			showCards(cells)
+    }
+}
+
+function showCards(csvLine){
+	for (var i = 0; i < csvLine[1]; i++) {
+		const card_url = GAMEWITH_URL+csvLine[0]+".png"
+		document.write("<img src="+card_url+">")
+	}
+}
