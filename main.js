857f41160daf1d71b7a9ee6ea9092207728577ac
fukayakenta <fukaya.kentan@gmail.com>
2024-11-12 14:01:04 +0900

    remove img by click

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const GAMEWITH_URL="https://img.gamewith.jp/article_tools/pokemon-tcg-pocket/gacha/"
 
+// 画像表示関連
 document.addEventListener('DOMContentLoaded', function() {
     fetch('card_list.csv')
         .then(response => response.text())
@@ -10,14 +11,31 @@ function parseCSV(data) {
     const lines = data.split('\n');
     for (let i = 0; i < lines.length; i++) {
         const cells = lines[i].split(',');
+        //＃をエスケープ
 		if (!cells[0].startsWith("#"))
 			showCards(cells)
     }
 }
 
 function showCards(csvLine){
+	let content_area = document.getElementById("content_area");
+
 	for (var i = 0; i < csvLine[1]; i++) {
-		const card_url = GAMEWITH_URL+csvLine[0]+".png"
-		document.write("<img src="+card_url+">")
+		const card_id = csvLine
+
+		var image = document.createElement('img');
+		image.src = GAMEWITH_URL+csvLine[0]+".png"
+		image.id = csvLine[0]+i
+		image.width = 272; // 横サイズ（px）
+		image.height = 379; // 横サイズ（px）
+		image.addEventListener('click', imgClick);
+		content_area.appendChild(image);
 	}
+}
+
+// 画像クリック
+function imgClick(e){
+	const id = e.target.getAttribute("id")
+	e.target.remove()
+	console.log(id)
 }
